c1c07c16db21b4552f4ef26e4120a4e1face6af1
Givontae <givontae.m2@gmail.com>
2025-03-31 20:56:20 -0400

    Update EmployeeManageable.java

--- src/government/school/staff/EmployeeManageable.java
+++ src/government/school/staff/EmployeeManageable.java
@@ -1,6 +1,21 @@
 package government.school.staff;
 
+/**
+ * Interface for managing employee operations such as hiring and firing.
+ */
 public interface EmployeeManageable {
+    
+    /**
+     * Terminates the employment of the specified employee.
+     *
+     * @param employee The employee to be fired.
+     */
     public void fireEmployee(Employee employee);
+    
+    /**
+     * Hires the specified employee.
+     *
+     * @param employee The employee to be hired.
+     */
     public void hireEmployee(Employee employee);
 }
